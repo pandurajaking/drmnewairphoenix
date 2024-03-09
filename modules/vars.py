43004185815ae394e8df6d8844acc684fcd01818
Ankush Yadav <138442615+Mswpresents@users.noreply.github.com>
2024-03-09 15:04:26 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
 api_id = 27815405
 api_hash = "4e70821cd2af3322f7cf2f2887e32821"
-bot_token = "7005117394:AAF023VgPlde__q1W_MtFsnnhTtBMZ2HIbo"
+bot_token = "6872534523:AAH9sblEb9jBRHR_oTiW9HaPQdehKGLGWg8"
 # Bot Username = "@gsdkkdbot"
