eaf10ae1ab0bbe474fac631006b1d21777ac79a6
Nickel18 <167849937+Nickel18@users.noreply.github.com>
2024-04-24 19:26:34 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
-api_id = 27815405
-api_hash = "4e70821cd2af3322f7cf2f2887e32821"
-bot_token = "6872534523:AAH9sblEb9jBRHR_oTiW9HaPQdehKGLGWg8"
-# Bot Username = "@gsdkkdbot"
+api_id = 20963688
+api_hash = "964a26dc404aa5e12dfe82ea94ae7ae3"
+bot_token = "7061439554:AAHDXBBktV5P4O6sjBLamdX2hhvBN7uPSl0"
+# Bot Username = "allinone"
